1f84a92a1a06627fd045b0d8334aeb1326237cc9
Elian Rodrigues <rodrigues.elian@yahoo.com>
2021-08-15 21:03:47 -0300

    fixed a little bug with login

--- Index.php
+++ Index.php
@@ -1,3 +1,40 @@
+<?php
+    include("funcoes_db.php");
+    session_start();
+    if(isset($_REQUEST['reqlogin']))
+    {
+        if($_REQUEST['reqlogin'] == 'login')
+        {
+            $login = $_POST['login'];
+            $senha = $_POST['senha'];
+        
+            $sql = "SELECT * from clientes where cliemail = '$login'";
+            $resultado = fazConsulta($sql);
+            if(password_verify($senha, $resultado[0]['clisenha'])){
+                $_SESSION['clicodig'] = $resultado[0]['clicodig'];
+                $_SESSION['usuario'] = $resultado[0]['cliemail'];
+                header('location: index.php');
+            }
+        }      
+        
+        if($_REQUEST['reqlogin'] == 'register')
+        {
+            $nome = $_POST['nome'];
+            $login = $_POST['login'];
+            $senha = $_POST['senha'];
+            $senhac = password_hash($senha, PASSWORD_DEFAULT);
+            $sql = "INSERT INTO clientes (clinome, cliemail, clisenha) VALUES ('$nome', '$login', '$senhac')";
+            $resultado = fazConsulta($sql);
+        }
+
+        if($_REQUEST['reqlogin'] == 'deslogar')
+        {
+            session_destroy();
+            header("Location: index.php");
+        }
+    }
+?>
+
 <html lang="en-US">
     <head>
         <meta charset="utf-8">
@@ -8,7 +45,6 @@
         <div id="tudo">
             <div class="loginbar"> 
             <?php 
-                session_start();
                 if(isset($_SESSION['usuario']))
                 {
                     $usuario = $_SESSION['usuario'];
@@ -28,14 +64,13 @@
                 <p>Games 4 You</p>
             </div>
             <div id="navbar">
-                <a style="margin-left: 1.1em;" href="?acao=home">Home</a>
+                <a href="?acao=home">Home</a>
                 <a href="?acao=products">Products</a>
                 <a href="?acao=contact">Contact</a>
                 <a href="?acao=about">About</a> 
             </div>
         </div>
         <?php
-            include("funcoes_db.php");
             $acao = "home";
             if(isset($_REQUEST['acao'])) 
             {
@@ -68,40 +103,3 @@
     <script src="scripts.js" type="text/javascript"></script>
 </html>
 
-<?php
-    if(isset($_REQUEST['reqlogin']))
-    {
-        if($_REQUEST['reqlogin'] == 'login')
-        {
-            $login = $_POST['login'];
-            $senha = $_POST['senha'];
-        
-            $sql = "SELECT * from clientes where cliemail = '$login'";
-            $resultado = fazConsulta($sql);
-            if(password_verify($senha, $resultado[0]['clisenha'])){
-                $_SESSION['clicodig'] = $resultado[0]['clicodig'];
-                $_SESSION['usuario'] = $resultado[0]['cliemail'];
-                header('location: index.php');
-            }
-            var_dump($resultado);
-        }      
-        
-        if($_REQUEST['reqlogin'] == 'register')
-        {
-            $nome = $_POST['nome'];
-            $login = $_POST['login'];
-            $senha = $_POST['senha'];
-            $senhac = password_hash($senha, PASSWORD_DEFAULT);
-            $sql = "INSERT INTO clientes (clinome, cliemail, clisenha) VALUES ('$nome', '$login', '$senhac')";
-            $resultado = fazConsulta($sql);
-        }
-
-        if($_REQUEST['reqlogin'] == 'deslogar')
-        {
-            session_destroy();
-            header("Location: index.php");
-        }
-    }
-    
-    
-?>
